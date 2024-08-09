c4f704d24e3710e9d3e3c3603f5bd514411e75f5
Adeolu Adeyemi <adeolu.ooa@gmail.com>
2024-08-09 15:54:13 +0100

    Assignment complete

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "classwork_iac_vpc_main" {
 
 # aws_subnet
 resource "aws_subnet" "classwork_iac_main_subnet" {
-    vpc_id     = aws_vpc.main.id
+    vpc_id     = aws_vpc.classwork_iac_vpc_main.id
     cidr_block = "10.0.1.0/24"
 
     tags = {
@@ -39,7 +39,7 @@ resource "aws_route_table" "classwork_iac_main_rt" {
     vpc_id = aws_vpc.classwork_iac_vpc_main.id
 
     route {
-        cidr_block = "10.0.1.0/24"
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.classwork_iac_gw.id
     }
 
@@ -59,10 +59,10 @@ resource "aws_route_table_association" "classwork_iac_main_rt_assoc_a" {
     route_table_id = aws_route_table.classwork_iac_main_rt.id
 }
 
-resource "aws_route_table_association" "classwork_iac_main_rt_assoc_b" {
-    gateway_id     = aws_internet_gateway.classwork_iac_gw.id
-    route_table_id = aws_route_table.classwork_iac_main_rt.id
-}
+# resource "aws_route_table_association" "classwork_iac_main_rt_assoc_b" {
+#     gateway_id     = aws_internet_gateway.classwork_iac_gw.id
+#     route_table_id = aws_route_table.classwork_iac_main_rt.id
+# }
 
 # aws_security_group
 resource "aws_security_group" "classwork_iac_sg_main" {
@@ -91,32 +91,11 @@ resource "aws_vpc_security_group_ingress_rule" "classwork_iac_sg_main_https" {
     to_port           = 443
 }
 
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-#     security_group_id = aws_security_group.allow_tls.id
-#     cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-#     from_port         = 443
-#     ip_protocol       = "tcp"
-#     to_port           = 443
-# }
-
 resource "aws_vpc_security_group_egress_rule" "classwork_iac_sg_main_http" {
-    security_group_id = aws_security_group.allow_tls.id
+    security_group_id = aws_security_group.classwork_iac_sg_main.id
     cidr_ipv4         = "0.0.0.0/0"
     ip_protocol       = "-1" # semantically equivalent to all ports
 }
-
-resource "aws_vpc_security_group_egress_rule" "classwork_iac_sg_main_https" {
-    security_group_id = aws_security_group.allow_tls.id
-    cidr_ipv4         = "0.0.0.0/0"
-    ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv6         = "::/0"
-#   ip_protocol       = "-1" # semantically equivalent to all ports
-# }
-
 
 # aws_key_pair
 resource "aws_key_pair" "deployer" {
@@ -141,13 +120,12 @@ data "aws_ami" "ubuntu" {
     owners = ["099720109477"] # Canonical
 }
 
-
-
 # aws_instance
 resource "aws_instance" "classwork_iac_sg_web" {
     ami           = data.aws_ami.ubuntu.id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [classwork_iac_sg_main.id]
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.classwork_iac_main_subnet.id
+    vpc_security_group_ids = [aws_security_group.classwork_iac_sg_main.id]
 
     tags = {
         Name = "classwork_iac_sg_main_instance"
